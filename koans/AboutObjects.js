ac6ab220a8fe10f32c41c99d5862e85e4c4901f7
George Chen <georgechen@Georges-MacBook-Air-2.local>
2019-02-27 17:15:07 -0800

    completed

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -8,12 +8,12 @@ describe("About Objects", function() {
     });
 
     it("should confirm objects are collections of properties", function() {
-      expect(meglomaniac.mastermind).toBe(FILL_ME_IN);
+      expect(meglomaniac.mastermind).toBe('Joker');
     }); 
 
     it("should confirm that properties are case sensitive", function() {
-      expect(meglomaniac.henchwoman).toBe(FILL_ME_IN);
-      expect(meglomaniac.henchWoman).toBe(FILL_ME_IN);
+      expect(meglomaniac.henchwoman).toBe('Harley');
+      expect(meglomaniac.henchWoman).toBe(undefined);
     });
   });
   
@@ -29,12 +29,16 @@ describe("About Objects", function() {
     };
    
     var battleCry = meglomaniac.battleCry(4);
-    expect(FILL_ME_IN).toMatch(battleCry);
+    expect('They are Pinky and the Brain Brain Brain Brain Brain').toMatch(battleCry);
+    //string matches string 
+    //.join convert it and concats to create a strings 
+    //'They are Pinky and the '
+    //[5] + ' Brain' => 5 Brain
   });
 
   it("should confirm that when a function is attached to an object, 'this' refers to the object", function () {
-    var currentDate = new Date();
-    var currentYear = (currentDate.getFullYear());
+    var currentDate = new Date(); //function called Date
+    var currentYear = (currentDate.getFullYear()); //
     var meglomaniac = { 
       mastermind: "James Wood", 
       henchman: "Adam West",
@@ -44,8 +48,8 @@ describe("About Objects", function() {
       }
     };
    
-    expect(currentYear).toBe(FILL_ME_IN);
-    expect(meglomaniac.calculateAge()).toBe(FILL_ME_IN);
+    expect(currentYear).toBe(2019);
+    expect(meglomaniac.calculateAge()).toBe(49); //2019 - 1970 = 49
   });
 
   describe("'in' keyword", function() {
@@ -61,26 +65,26 @@ describe("About Objects", function() {
     it("should have the bomb", function() {
       var hasBomb = "theBomb" in meglomaniac;
      
-      expect(hasBomb).toBe(FILL_ME_IN);
+      expect(hasBomb).toBe(true);
     });
 
     it("should not have the detonator however", function() {
       var hasDetonator = "theDetonator" in meglomaniac;
      
-      expect(hasDetonator).toBe(FILL_ME_IN);
+      expect(hasDetonator).toBe(false);
     });    
   });
 
   it("should know that properties can be added and deleted", function() {
     var meglomaniac = { mastermind : "Agent Smith", henchman: "Agent Smith" };
 
-    expect("secretary" in meglomaniac).toBe(FILL_ME_IN);
+    expect("secretary" in meglomaniac).toBe(false);
 
     meglomaniac.secretary = "Agent Smith";
-    expect("secretary" in meglomaniac).toBe(FILL_ME_IN);
+    expect("secretary" in meglomaniac).toBe(true);
     
-    delete meglomaniac.henchman;
-    expect("henchman" in meglomaniac).toBe(FILL_ME_IN);
+    delete meglomaniac.henchman; //deleted the prop so it is false 
+    expect("henchman" in meglomaniac).toBe(false);
   });
 
 
@@ -89,19 +93,20 @@ describe("About Objects", function() {
     {
       this.radius = radius;
     }
-
-    var simpleCircle = new Circle(10);
-    var colouredCircle = new Circle(5);
-    colouredCircle.colour = "red";
-    
-    expect(simpleCircle.colour).toBe(FILL_ME_IN);
-    expect(colouredCircle.colour).toBe(FILL_ME_IN);
+    //function that takes in a radius and is an object 
+    //this refers to the prop name within the scope 
+    var simpleCircle = new Circle(10); // simpleCircle = {radius : 10}
+    var colouredCircle = new Circle(5); // colouredCircle = {radius : 5}
+    colouredCircle.colour = "red"; //added colour prop with red key into colouredCircle obj with 
+    // colouredCircle = {radius: 10, colour: red}
+    expect(simpleCircle.colour).toBe(undefined); //because colour prop does not exidst in simpleCircle obj
+    expect(colouredCircle.colour).toBe('red');
   
     Circle.prototype.describe = function() {
       return "This circle has a radius of: " + this.radius;
     };
   
-    expect(simpleCircle.describe()).toBe(FILL_ME_IN);
-    expect(colouredCircle.describe()).toBe(FILL_ME_IN);
+    expect(simpleCircle.describe()).toBe('This circle has a radius of: 10'); //describe included about simpleCircle
+    expect(colouredCircle.describe()).toBe('This circle has a radius of: 5');
   });
 });
