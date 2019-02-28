498f5e2bc5c51ad45a562898fedb389acd890da7
George Chen <georgechen@Georges-MacBook-Air-2.local>
2019-02-28 00:40:35 -0800

    completed

--- koans/AboutInheritance.js
+++ koans/AboutInheritance.js
@@ -21,7 +21,7 @@ SwedishChef.prototype = new Muppet();
 describe("About inheritance", function() {
   beforeEach(function(){
     this.muppet      = new Muppet(2, "coding"); //{age : 2, hobby: coding, answerNanny: Everything's cool}
-  	this.swedishChef = new SwedishChef(2, "cooking", "chillin"); //{{age : 2, hobby: coding, answerNanny: Everything's cool}, mood: chilling, cook: 'Mmmm soup!'}
+  	this.swedishChef = new SwedishChef(2, "cooking", "chillin"); //{{age : 2, hobby: cooking, answerNanny: Everything's cool}, mood: chilling, cook: 'Mmmm soup!'}
   });
   
   it("should be able to call a method on the derived object", function() {
@@ -34,7 +34,7 @@ describe("About inheritance", function() {
   
   it("should set constructor parameters on the base object", function() {
     expect(this.swedishChef.age).toEqual(2);
-    expect(this.swedishChef.hobby).toEqual('coding');
+    expect(this.swedishChef.hobby).toEqual('cooking');
   });
   
   it("should set constructor parameters on the derived object", function() {
@@ -43,14 +43,14 @@ describe("About inheritance", function() {
 });
 
 // http://javascript.crockford.com/prototypal.html
-Object.prototype.beget = function () {
-  function F() {}
-  F.prototype = this;
-  return new F();
+Object.prototype.beget = function () { //add new method into Object constructor
+  function F() {} 
+  F.prototype = this; //add this into F function
+  return new F(); //return F constructor with this as key 
 }
 
 function Gonzo(age, hobby, trick) {
-  Muppet.call(this, age, hobby);
+  Muppet.call(this, age, hobby); 
   this.trick = trick;
   
   this.doTrick = function() {
@@ -60,6 +60,9 @@ function Gonzo(age, hobby, trick) {
 
 // no longer need to call the Muppet (base type) constructor
 Gonzo.prototype = Muppet.prototype.beget();
+//Muppet.prototype.beget() = function () {
+    //function 
+//}
 // note: if you're wondering how this line affects the below tests, the answer is that it doesn't.
 // however, it does do something interesting -- it makes this work:
 // var g = new Gonzo(...);
@@ -71,19 +74,25 @@ describe("About Crockford's inheritance improvement", function() {
   });
   
   it("should be able to call a method on the derived object", function() {
-    expect(this.gonzo.doTrick()).toEqual(FILL_ME_IN);
+    expect(this.gonzo.doTrick()).toEqual('eat a tire');
+    //Gonzo(3, 'daredevil performer', 'eat a tire').doTrick()
+    //
   });
   
   it("should be able to call a method on the base object", function() {
-    expect(this.gonzo.answerNanny()).toEqual(FILL_ME_IN);
+    expect(this.gonzo.answerNanny()).toEqual("Everything\'s cool!");
+    //Gonzo(3, 'daredevil performer', 'eat a tire').answerNanny()
+    //this answerNanny = 'Everything's cool'
   });
   
   it("should set constructor parameters on the base object", function() {
-    expect(this.gonzo.age).toEqual(FILL_ME_IN);
-    expect(this.gonzo.hobby).toEqual(FILL_ME_IN);
+    expect(this.gonzo.age).toEqual(3);
+    //new Gonzo(3, "daredevil performer", "eat a tire").age 
+    //3
+    expect(this.gonzo.hobby).toEqual('daredevil performer');
   });
   
   it("should set constructor parameters on the derived object", function() {
-    expect(this.gonzo.trick).toEqual(FILL_ME_IN);
+    expect(this.gonzo.trick).toEqual('eat a tire');
   });
 });
